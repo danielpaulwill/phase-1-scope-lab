25c17cd680664ed82e918c99bf8907dd3649e45f
Daniel Williams <danielpaulwill@gmail.com>
2021-07-30 14:13:11 -0400

    commit tree d7621d3c3e4bb343c821bba6d0c2bf115d11fe1c

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+const leastFavoriteCustomer = 'patrick'
+
+function upperCaseCustomerName() {
+   return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+    return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+    return bestCustomer;
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'dave';
+}
+
+console.log(changeLeastFavoriteCustomer());
